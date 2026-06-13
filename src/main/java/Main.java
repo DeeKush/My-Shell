bce5dc180ae55fe8888caaba99ef645de3161c45
Deepak <deepak@example.com>
2026-06-13 22:25:31 +0530

    add type command support for shell builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
+
+    private static boolean isBuiltin(String cmd) {
+        return cmd.equals("exit")
+                || cmd.equals("echo")
+                || cmd.equals("type");
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,6 +20,14 @@ public class Main {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (isBuiltin(command)) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
